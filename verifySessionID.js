d095ef455298439d117b8c427e6eb97d26da98ef
Steven <SteveMB1@users.noreply.github.com>
2016-12-27 03:54:50 -0500

    Update verifySessionID.js

--- verifySessionID.js
+++ verifySessionID.js
@@ -1,20 +1,16 @@
-var express=require('express');
-var router=express.Router();
-var AWS=require('aws-sdk');
-var cookieParser=require('cookie-parser');
-var bodyParser=require('body-parser');
-var crypto = require('crypto');
+const express=require('express');
+const router=express.Router();
+const AWS=require('aws-sdk');
+const cookieParser=require('cookie-parser');
+const bodyParser=require('body-parser');
 "use strict";
-
 /* AWS CONF. BOL */
-AWS.config.update({region:"us-east-1",accessKeyId:'',secretAccessKey:''});
-var docClient=new AWS.DynamoDB.DocumentClient();
-var table="AuthenticationSessionKeys";
+AWS.config.update({region:"",accessKeyId:'',secretAccessKey:''});
+const docClient=new AWS.DynamoDB.DocumentClient();
+const table="AuthenticationSessionKeys";
 
-// module.exports = {
-checkSessionID: function checkSessionID(sessionID_from_header){
-
-var sessionID_search = sessionID_from_header
+/* exports.checkSessionID = */ function checkSessionID(sessionID_from_header){
+var sessionID_search = sessionID_from_header.toString()
   //Read for current SessionID.
   var params = {
     TableName: table,
@@ -27,17 +23,12 @@ var sessionID_search = sessionID_from_header
     if (err) {
         console.log({"Error":{"Critical":"Unable to process item. "+err}});
     } else {
-      for(sessionID_search in data){
-        var result_from_db = data.Item.SessionId;
-        if (result_from_db === sessionID_from_header){
-          console.log("SessionID checks out to be OK - Returning to next statement in submitting function");
-          return;
-        } else {res.json({"Warning":{"Response":"Login failed, credentials are incorrect."}}); break;}
-        // Commit IP to the BLOCK DB after 10 attempts
-      // }
-        }
-  };
+        if (data.Item.SessionId === sessionID_from_header){
+          console.log("SessionID checks out to be OK - Returning to next statement in submitting function"); return;
+          //define type error
+        };
+      };
 });
-  } //End of checkSessionID function
-
+} //End of checkSessionID function
 // }; //end of module
+// module.exports = checkSessionID;
